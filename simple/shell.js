fddae460b696231c744f9b37268f28c909433c19
Lennart Kats <lclkats@gmail.com>
2013-10-07 10:50:15 +0200

    Specify a completion regex for shell scripts

---
+++ simple/shell.js
@@ -0,0 +1,21 @@
+define(function(require, exports, module) {
+
+var baseLanguageHandler = require('plugins/c9.ide.language/base_handler');
+
+var handler = module.exports = Object.create(baseLanguageHandler);
+    
+handler.handlesLanguage = function(language) {
+    return language === "sh";
+};
+
+handler.getIdentifierRegex = function() {
+    // Allow slashes for paths, dots, dashes, no dollars
+    return (/[a-zA-Z_0-9\/\.\-]/);
+};
+
+handler.getCompletionRegex = function() {
+    return (/\$/);
+};
+
+
+});
