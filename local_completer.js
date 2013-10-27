4f5be1e45fb934744183cd8955e53b803b773e08
Lennart Kats <lclkats@gmail.com>
2013-10-27 16:44:35 +0100

    Tweak completion scores to show local completion of current item first
    (assuming complete.js decides to show it)

--- local_completer.js
+++ local_completer.js
@@ -4,6 +4,7 @@ var baseLanguageHandler = require('plugins/c9.ide.language/base_handler');
 var completeUtil = require("plugins/c9.ide.language/complete_util");
 
 var SPLIT_REGEX = /[^a-zA-Z_0-9\$]+/;
+var MAX_SCORE = 1000000;
 
 var completer = module.exports = Object.create(baseLanguageHandler);
     
@@ -41,9 +42,9 @@ function wordDistanceAnalyzer(doc, pos, prefix) {
         var distance = Math.max(prefixPosition, i) - Math.min(prefixPosition, i);
         // Score substracted from 100000 to force descending ordering
         if (Object.prototype.hasOwnProperty.call(identDict, ident))
-            identDict[ident] = Math.max(1000000-distance, identDict[ident]);
+            identDict[ident] = Math.max(MAX_SCORE - distance, identDict[ident]);
         else
-            identDict[ident] = 1000000-distance;
+            identDict[ident] = MAX_SCORE - distance;
         
     }
     return identDict;
@@ -69,6 +70,7 @@ completer.complete = function(doc, fullAst, pos, currentNode, callback) {
     var line = doc.getLine(pos.row);
     var regex = this.$getIdentifierRegex();
     var identifier = completeUtil.retrievePrecedingIdentifier(line, pos.column, regex);
+    var fullIdentifier = identifier + completeUtil.retrieveFollowingIdentifier(line, pos.column, regex);
          
     var allIdentifiers = [];
     for (var ident in identDict) {
@@ -85,7 +87,7 @@ completer.complete = function(doc, fullAst, pos, currentNode, callback) {
           name        : m,
           replaceText : m,
           icon        : null,
-          score       : identDict[m],
+          score       : m === fullIdentifier ? MAX_SCORE : identDict[m],
           meta        : "",
           isGeneric   : true,
           priority    : 0
