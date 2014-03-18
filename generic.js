a8facb531d22713026985ba03df01db8d7640c7f
Lennart Kats <lclkats@gmail.com>
2014-03-18 16:22:52 +0100

    Add outline view, occurrence highlighting, etc. for Makefiles

--- generic.js
+++ generic.js
@@ -17,6 +17,7 @@ define(function(require, exports, module) {
         language.registerLanguageHandler("plugins/c9.ide.language.generic/mode_completer");
         language.registerLanguageHandler('plugins/c9.ide.language.generic/open_files_local_completer');
         language.registerLanguageHandler('plugins/c9.ide.language.generic/simple/shell');
+        language.registerLanguageHandler('plugins/c9.ide.language.generic/simple/make');
         
         register(null, {});
     }
