6801590fc1c77f00d39b9492171de681373fa1c9
nightwing <amirjanyan@gmail.com>
2015-02-15 02:00:58 +0400

    remove incorrect license information from comments

--- generic.js
+++ generic.js
@@ -2,7 +2,6 @@
  * Code completion for the Cloud9
  *
  * @copyright 2010, Ajax.org B.V.
- * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
 define(function(require, exports, module) {
     main.consumes = ["language"];
