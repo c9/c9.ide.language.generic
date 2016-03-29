bd4acfc645b42349d92586aea2d76e85615e7bec
Lennart Kats <lclkats@gmail.com>
2016-03-29 13:16:05 +0000

    Use CSS mode_completer only in SSH mode

--- generic.js
+++ generic.js
@@ -14,7 +14,8 @@ define(function(require, exports, module) {
 
         language.registerLanguageHandler('plugins/c9.ide.language.generic/local_completer');
         language.registerLanguageHandler('plugins/c9.ide.language.generic/snippet_completer');
-        language.registerLanguageHandler("plugins/c9.ide.language.generic/mode_completer");
+        if (options.mode_completer)
+            language.registerLanguageHandler("plugins/c9.ide.language.generic/mode_completer");
         language.registerLanguageHandler('plugins/c9.ide.language.generic/open_files_local_completer');
         language.registerLanguageHandler('plugins/c9.ide.language.generic/simple/shell');
         language.registerLanguageHandler('plugins/c9.ide.language.generic/simple/make');
